e6dfbe87e5752ed15f50edc179292ba507ea116e
Brian Nanzer <brian.nanzer@gmail.com>
2024-01-10 21:39:51 -0300

    Implementación básica de controles del cohete

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -1,19 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float rotationSpeed = 1f;
+    [SerializeField] private float thrustSpeed = 1f;
+    private float rotationDirection;
+    private bool isRotating = false;
+    private bool isThrusting = false;
+    private Rigidbody2D rigidBody;
+
+    private void Awake(){
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    private void Update(){
+        if(isRotating){
+            Rotate();
+        }
+
+        if(isThrusting){
+            Thrust();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Rotate(){
+        rigidBody.AddTorque(rotationDirection * rotationSpeed * Time.deltaTime);
+        Debug.Log("Rotating");
+    }
+
+    private void Thrust(){
+        rigidBody.AddRelativeForce(Vector2.up * thrustSpeed * Time.deltaTime);
+        Debug.Log("Thrusting");
+    }
+
+    private void OnRotate(InputValue value){
+        rotationDirection = value.Get<Vector2>().x * -1;
+        isRotating = rotationDirection != 0;
+    }
+
+    private void OnThrust(InputValue input){
+        isThrusting = input.isPressed;
     }
 }
