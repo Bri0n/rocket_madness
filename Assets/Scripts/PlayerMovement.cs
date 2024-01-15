a49581c5bb4d334bea8742529598cbb5956265cf
Brian Nanzer <brian.nanzer@gmail.com>
2024-01-14 22:36:44 -0300

    ParticleEffect de Thrust y pulido de movimiento

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,7 +9,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float rotationSpeed = 1f;
     [SerializeField] private float thrustSpeed = 1f;
     [SerializeField] private GameObject directionIndicator;
-    [SerializeField] private Vector2 indicatorDistance = new Vector2(1f, 1.5f);
+    [SerializeField] private Vector2 thrustIndicatorDistance = new Vector2(1f, 1.5f);
+    [SerializeField] private ParticleSystem thrustEffect;
     //private float rotationDirection;
     private Vector2 thrustDirection = new Vector2(0, 1);
     private bool isRotating = false;
@@ -20,42 +21,41 @@ public class PlayerMovement : MonoBehaviour
         rigidBody = GetComponent<Rigidbody2D>();
     }
     private void Update(){
-        /*if(isRotating){
-            Rotate();
-        }*/
-        directionIndicator.transform.position = (Vector2)transform.position + thrustDirection;
         if(isThrusting){
             Thrust();
+            if(!thrustEffect.isPlaying){
+                thrustEffect.Play();
+            }
+            Debug.Log("Thrusting");
+        } else {
+            if(thrustEffect.isPlaying){
+                thrustEffect.Stop();
+            }
         }
     }
-
-    /*private void Rotate(){
-        
-        rigidBody.freezeRotation = true;
-        //float impulse = rotationDirection * rotationSpeed * Time.deltaTime;
-        //rigidBody.AddTorque(impulse);
-        rigidBody.rotation += rotationDirection * rotationSpeed * Time.deltaTime;
-        Debug.Log("Rotating");
-        rigidBody.freezeRotation = false;
-    }*/
-
-
 
     private void Thrust(){
         rigidBody.AddRelativeForce(thrustDirection * thrustSpeed * Time.deltaTime);
-        Debug.Log("Thrusting");
     }
 
     private void OnRotate(InputValue value){
-        /*rotationDirection = value.Get<Vector2>().x * -1;
-        isRotating = rotationDirection != 0;*/
         Vector2 newDirection = value.Get<Vector2>();
-        if(newDirection != Vector2.zero){
-            thrustDirection = newDirection * indicatorDistance;
-            directionIndicator.transform.rotation = quaternion.identity;
-            directionIndicator.transform.Rotate(0, 0, Vector2.SignedAngle(new Vector2(0,1),newDirection));
+        if(newDirection != Vector2.zero)
+        {
+            thrustDirection = newDirection * thrustIndicatorDistance;
+            UpdateMovementEffects(newDirection);
         }
-        
+
+    }
+
+    private void UpdateMovementEffects(Vector2 newDirection)
+    {
+        Transform indicatorTransform= directionIndicator.transform;
+        indicatorTransform.position = (Vector2)transform.position + thrustDirection * thrustIndicatorDistance;
+        indicatorTransform.rotation = quaternion.identity; // Reiniciar rotación así no se acumula
+        indicatorTransform.Rotate(0, 0, Vector2.SignedAngle(Vector2.up, newDirection));
+        thrustEffect.transform.position = (Vector2)transform.position + thrustDirection * -1;
+        thrustEffect.transform.rotation = directionIndicator.transform.rotation;
     }
 
     private void OnThrust(InputValue input){
