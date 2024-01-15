7489172f3f76636818ae8d42453293a21a6ac1a7
Brian Nanzer <brian.nanzer@gmail.com>
2024-01-14 22:03:08 -0300

    Nuevo sistema de movimiento direccional sin rotación

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
@@ -7,7 +8,10 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 1f;
     [SerializeField] private float thrustSpeed = 1f;
-    private float rotationDirection;
+    [SerializeField] private GameObject directionIndicator;
+    [SerializeField] private Vector2 indicatorDistance = new Vector2(1f, 1.5f);
+    //private float rotationDirection;
+    private Vector2 thrustDirection = new Vector2(0, 1);
     private bool isRotating = false;
     private bool isThrusting = false;
     private Rigidbody2D rigidBody;
@@ -16,15 +20,16 @@ public class PlayerMovement : MonoBehaviour
         rigidBody = GetComponent<Rigidbody2D>();
     }
     private void Update(){
-        if(isRotating){
+        /*if(isRotating){
             Rotate();
-        }
+        }*/
+        directionIndicator.transform.position = (Vector2)transform.position + thrustDirection;
         if(isThrusting){
             Thrust();
         }
     }
 
-    private void Rotate(){
+    /*private void Rotate(){
         
         rigidBody.freezeRotation = true;
         //float impulse = rotationDirection * rotationSpeed * Time.deltaTime;
@@ -32,18 +37,25 @@ public class PlayerMovement : MonoBehaviour
         rigidBody.rotation += rotationDirection * rotationSpeed * Time.deltaTime;
         Debug.Log("Rotating");
         rigidBody.freezeRotation = false;
-    }
+    }*/
 
 
 
     private void Thrust(){
-        rigidBody.AddRelativeForce(Vector2.up * thrustSpeed * Time.deltaTime);
+        rigidBody.AddRelativeForce(thrustDirection * thrustSpeed * Time.deltaTime);
         Debug.Log("Thrusting");
     }
 
     private void OnRotate(InputValue value){
-        rotationDirection = value.Get<Vector2>().x * -1;
-        isRotating = rotationDirection != 0;
+        /*rotationDirection = value.Get<Vector2>().x * -1;
+        isRotating = rotationDirection != 0;*/
+        Vector2 newDirection = value.Get<Vector2>();
+        if(newDirection != Vector2.zero){
+            thrustDirection = newDirection * indicatorDistance;
+            directionIndicator.transform.rotation = quaternion.identity;
+            directionIndicator.transform.Rotate(0, 0, Vector2.SignedAngle(new Vector2(0,1),newDirection));
+        }
+        
     }
 
     private void OnThrust(InputValue input){
